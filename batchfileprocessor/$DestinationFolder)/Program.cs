d5022c9dc285984b37a965ef72fbdb7467365853
Jeffery-Liu <liujinfeng1209@gmail.com>
2015-11-30 20:09:07 -0800

    first commit

---
+++ batchfileprocessor/$DestinationFolder)/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BatchFileFramework;
+namespace batchFileProcessor
+{
+	class Program
+	{
+		static long totalFilesLen = 0;
+
+		static void testNotify(String msg)
+		{
+			Console.WriteLine(msg);
+		}
+
+		static void findTotalFilesLength(IFileAccessLogic lo, System.IO.FileInfo fi)
+		{
+			totalFilesLen += fi.Length;
+		}
+
+		static void Main(string[] args)
+		{
+			if(args.Length < 1)
+			{
+				Console.WriteLine("Missing file or folder path: \nUsage: batchFileProcessor filepath(folderpath)");
+				return;
+			}
+
+			FileAccessLogic accessor = new FileAccessLogic();
+			accessor.OnNotify += testNotify;
+			accessor.OnProcess += findTotalFilesLength;
+
+			accessor.Recursive = true;
+			accessor.Execute(args[0]);
+
+			Console.WriteLine("Total Files length in directory {0} is {1}", args[0], totalFilesLen);
+			// Keep the console window open in debug mode.
+			Console.WriteLine("Press any key to exit.");
+			Console.ReadKey();
+
+		}		       
+
+	}
+}
